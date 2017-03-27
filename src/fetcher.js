cd7d06a0c61d17653485ef1964e32114a65ca728
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2017-03-27 19:45:03 +0900

    fix xhr params

--- src/fetcher.js
+++ src/fetcher.js
@@ -1,7 +1,7 @@
 export default url => {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
-    xhr.open(url);
+    xhr.open("get", url);
     xhr.send();
     xhr.addEventListener("load", () => resolve(xhr.responseText));
     xhr.addEventListener("error", e => reject(e));
